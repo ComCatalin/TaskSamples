3194cdfa32590177f54bc743ab83f51fd2b8d35e
Catalin-Comanescu <com.catalin@hotmail.com>
2019-05-04 12:29:11 +0200

    Foreground task sample

--- TaskSamples/ForegroundTask/Program.cs
+++ TaskSamples/ForegroundTask/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ForegroundTask
 {
@@ -6,7 +8,58 @@ namespace ForegroundTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task fgTask = CreateForegroundTask<int>(() =>
+            {
+                Console.WriteLine("Running...");
+                Thread.Sleep(2000);
+                return 42;
+            }).ContinueWith(
+                t => Console.WriteLine($"Result is {t.Result}"), 
+                TaskContinuationOptions.ExecuteSynchronously);
+            Console.WriteLine("Main Completed.");
+        }
+
+        public static Task<T> CreateForegroundTask<T>(Func<T> taskBody)
+        {
+            return CreateForegroundTask(taskBody, CancellationToken.None);
+        }
+
+        public static Task<T> CreateForegroundTask<T>(Func<T> taskBody, CancellationToken ct)
+        {
+            var tcs = new TaskCompletionSource<T>();
+            var fgThread = new Thread(() => ExecuteForegroundTaskBody(taskBody, ct, tcs));
+            fgThread.Start();
+            // Return a task that is bound to the lifetime of the foreground thread body
+            return tcs.Task;
+        }
+
+        private static void ExecuteForegroundTaskBody<T>(
+            Func<T> taskBody, 
+            CancellationToken ct, 
+            TaskCompletionSource<T> tcs)
+        {
+            try
+            {
+                T result = taskBody();
+                tcs.SetResult(result);
+            }
+            catch(OperationCanceledException cancelledException)
+            {
+                // Cancellation is associated with the cancellation token for this Task
+                if (ct == cancelledException.CancellationToken)
+                {
+                    tcs.SetCanceled();
+                }
+                else
+                {
+                    tcs.SetException(cancelledException);
+                }
+            }
+            catch(Exception exc)
+            {
+                // Set the task status to Faulted, and re-throw as part of an AggregateException
+                tcs.SetException(exc);
+            }
         }
     }
 }
